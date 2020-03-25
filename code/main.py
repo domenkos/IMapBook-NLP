ef1f85f0c5dd5fb13e3470216f1bcf5cff911da5
domenkos <domen.kos@live.com>
2020-03-25 12:06:19 +0100

    template for report

--- code/main.py
+++ code/main.py
@@ -1,1 +1,6 @@
-print( 'Hello World' )
+import pandas as pd
+
+DATA_DIR = '../data/'
+FILENAME = 'AllDiscussionDataCODED_USE_THIS_14Feb2020_MH.xls'
+
+data = df = pd.read_excel( io=DATA_DIR + FILENAME ).dropna( 'columns', 'all' )
