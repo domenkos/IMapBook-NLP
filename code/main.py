39eb617ccf5b392b8cafbd851fff4f3468a1bd50
domenkos <domen.kos@live.com>
2020-05-24 20:52:45 +0200

    final commit

--- code/main.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-DATA_DIR = '../data/'
-FILENAME = 'AllDiscussionDataCODED_USE_THIS_14Feb2020_MH.xls'
-
-data = df = pd.read_excel( io=DATA_DIR + FILENAME ).dropna( 'columns', 'all' )
